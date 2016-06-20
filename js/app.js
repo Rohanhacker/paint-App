d99438ec8e1c6b11339e558d9aba07a519652128
rohanhacker <rohan.root@gmail.com>
2016-06-20 12:13:08 +0530

    color selector working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var $color = $(".red").css("background-color");
 $("#revealColorSelect").click(function() {
   $("#colorSelect").toggle();
 });
@@ -5,4 +6,16 @@ $("#revealColorSelect").click(function() {
 $(".controls ul").on("click","li",function() {
   $(".selected").removeClass("selected");
   $(this).addClass("selected");
+  $color = $(this).css("background-color");
 });
+function colorshow() {
+  var $r = $("#red").val();
+  var $g = $("#green").val();
+  var $b = $("#blue").val();
+  $("#newColor").css({"background-color": "rgb(" + $r + "," + $g + "," + $b + ")" });
+}
+colorshow();
+$("input[type=range]").mouseenter(function() {
+    $(this).mousemove(colorshow);
+}
+);
